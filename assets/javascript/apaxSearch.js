9bd20c4ec3803e2d8cf5f26e51e4f352813971d2
Alferguson <alferguson@berkeley.edu>
2017-10-22 22:17:53 -0700

    almost there

--- assets/javascript/apaxSearch.js
+++ assets/javascript/apaxSearch.js
@@ -1,38 +1,66 @@
 // array of nintendo characters that will have buttons at each refresh of page
-var nintendoArr = ["Link", "Mario", "Luigi", "Samus", "Ice Climbers", "Toad"];
+var nintendoArr = ["Link", "Mario", "Luigi", "Ice Climbers", "Toad"];
 
-var nintendoButton = $("<button type='button'>");
-
+var nintendoButton;
+var gifImg;
 var querySearch;
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + querySearch + "api_key=dc6zaTOxFJmzC&limit=10";
+var queryURL;
 
+window.onload = function() {
+	for (var i=0;i<nintendoArr.length;i++) {
+			var nintendoButton = $("<button type='button'>");
+		
+
+			nintendoButton.addClass("btn btn-outline-info btn-sm");
+		
+
+			nintendoButton.attr("id", nintendoArr[i] + "Button");
+		
+			
+
+			nintendoButton.text(nintendoArr[i]);
+		
+
+			$("#nintendo-buttons-group").append(nintendoButton);
+		
+		}
+}		
 
 // function to dynamically create buttons from nintendoArr
-function buttonCreater() {
+var buttonCreater = function() {
 
 	event.preventDefault();
 
-	buttonName = $("#submit-button").val().trim();
+	buttonName = $("#giphy-form").val().trim();
 
-	nintendoArr.push(buttonName);
+	// if user didn't input anything, don't create a button out of nothing
+	if (buttonName != "") {
 
-	nintendoButton.addClass("btn btn btn-outline-info btn-sm");
+		nintendoArr.push(buttonName);
 
-	nintendoButton.attr("id", buttonName + "Button");
-
-	nintendoButton.text(buttonName);
-
-	$("#nintendo-buttons-group").append(nintendoButton);
+		$("#nintendo-buttons-group").empty();
 
 
-	$.ajax({
-		  url: queryURL,
-		  method: 'GET'
-		}).done(function(result) {
-		  console.log(result);
 
-		 
-		});
+		for (var i=0;i<nintendoArr.length;i++) {
+			var nintendoButton = $("<button type='button'>");
+		
+
+			nintendoButton.addClass("btn btn-outline-info btn-sm");
+		
+
+			nintendoButton.attr("id", nintendoArr[i] + "Button");
+		
+			
+
+			nintendoButton.text(nintendoArr[i]);
+		
+
+			$("#nintendo-buttons-group").append(nintendoButton);
+		
+		}
+
+	}
 }
 // Start of page or submit button click, dynamically creates buttons from nintendoArr by putting index into queryURL and pushes input value into the array
 $("#submit-button").on("click", buttonCreater) 
@@ -41,23 +69,70 @@ $("#submit-button").on("click", buttonCreater)
 
 // nintendoArr button click, appends 10 gifs to id="gifs" of the name of that name
 
-$("#nintendo-buttons").on("click", function() { 
+$(document).on("click", ".btn-sm", function() { 
+	
+	event.preventDefault();
 
 	// function to empty gifs-group so new nintendoArr button click empties previous gifs
 	$("#gifs-group").empty();
 
-	event.preventDefault();
-    // This line grabs the input from the textbox
-    var movie = $("#movie-input").val().trim();
+	
+    // This line grabs the name of the button and removes spaces with +'s, can use regexp here??
+    querySearch = $(this).text().split(" ").join('+');
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + querySearch + "&api_key=lhoLYl1nq480N9fUdxllWCnMrjfq9Kei&limit=10";
 	$.ajax({
 	  url: queryURL,
 	  method: 'GET'
 	}).done(function(result) {
 	  console.log(result);
 
+	  for(var j=0;j<result.data.length;j++) {
+
+		  gifImg = $("<img>");
+		  gifImg.attr({
+		  	"data-still": result.data[j].images.original_still.url,
+		  	"data-animate": result.data[j].images.original.url,
+		  	// technically, state and src don't need quotations but I like to keep it consistent between the keys
+		  	"data-state": "still",
+		  	"src": result.data[j].images.original_still.url 
+		  })
+		  // gifImg.attr("src", result.data[j].images.original_still.url);
+		  gifImg.addClass("gif");
+		  $("#gifs-group").append(gifImg);
+		}
 	 
 	});
 })
-// gif is clicked, animate them, and if clicked again, they will go back to initial frame
-$("#gifs").on("click", function());
 
+$(document).on("click", ".gif", function() { 
+	
+	state = $(this).data("state");
+	still = $(this).data("still");
+	animate = $(this).data("animate");
+	src = $(this).context.src;
+	debugger;
+	if ($(this).attr("data-state") == "still") {
+		debugger;
+		state = "animate";
+		src = animate;
+		$(this).attr({
+			"data-state": state,
+			"src": src
+		});
+		
+		// $(this).context.src = src;
+	}
+	else if ($(this).attr("data-state") == "animate") {
+		state = "still";
+		src = still;
+		$(this).attr({
+			"data-state": state,
+			"src": src
+		});
+		// $(this).data("state") = state;
+		
+		// $(this).context.src = src;
+	}
+
+})
